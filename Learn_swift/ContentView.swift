6a662340013ce2be9bdb26ddc8e6fc7e232c063d
simjae <sjhwo9370@gmail.com>
2022-02-13 16:19:47 +0900

    스위프트UI 레이아웃 및 사용법 공부

--- Learn_swift/ContentView.swift
+++ Learn_swift/ContentView.swift
@@ -1,21 +1,63 @@
 //
 //  ContentView.swift
-//  Learn_swift
+//  SwiftUI#1
 //
 //  Created by MACBOOK PRO on 2022/02/13.
 //
 
 import SwiftUI
 
+
 struct ContentView: View {
+    //이안에서만 사용 private
+    //값이 변할수있는 var
+    //@State 값의 변화를 감지 >> 다시렌더링을해서 뷰에 적용
+    
+    @State
+    private var isActivated : Bool = false
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding().background(.red
-            )
+        
+        NavigationView{
+            VStack{
+                Text("심재새내기")
+                HStack{
+                    MyVstackView()
+                    MyVstackView()
+                    MyVstackView()
+                    
+                }//HStack
+                    .padding(isActivated ? 50.0 : 10.0).background(isActivated ? Color.yellow : Color.black)
+                    .onTapGesture {
+                        print("HStack이  클릭되었다.")
+                        
+                        
+                        withAnimation{
+                            self.isActivated.toggle()
+                        }
+                    
+                }//HStack
+                
+                //네이게이션 버튼(링크)
+                
+                //destination: 다음에 보여줄화면
+                NavigationLink(destination:MyNextView()){
+                    Text("네비게이션")
+                        .fontWeight(.bold)
+                        .padding()
+                        .background(.orange)
+                        
+                }
+            }
+          
+        }//NavigationView
+      
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    //뷰확인용
     static var previews: some View {
         ContentView()
     }
