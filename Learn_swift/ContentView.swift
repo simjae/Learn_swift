92d2b681c2ff707b8b2295a598c4a7cc36cb37c4
simjae <sjhwo9370@gmail.com>
2022-02-13 16:04:44 +0900

    playground   start

--- Learn_swift/ContentView.swift
+++ Learn_swift/ContentView.swift
@@ -10,7 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
-            .padding()
+            .padding().background(.red
+            )
     }
 }
 
---
+++ Learn_swift/MyPlayground.playground/Contents.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+var greeting = "Hello, playground"
+var i : Int = 100
+print(greeting)
+print(i)
